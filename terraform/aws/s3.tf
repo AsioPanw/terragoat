6e427595da5b01e7afb05e828b1cec2125fe972e
Armand Siorak <armand.siorak@gmail.com>
2023-01-11 17:10:40 +0100

    Adding a new bucket

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -63,6 +63,16 @@ resource "aws_s3_bucket" "financials" {
 
 }
 
+resource "aws_s3_bucket" "demoeffect" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+
+}
+
 resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
